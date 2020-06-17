186d510f9084b93f411e726a02a9f748c4357518
Priime <lucas.stamaria@gmail.com>
2020-06-17 11:15:30 +0800

    added routing to retrieve css&js files

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const FRONTEND = '/../frontend';
+const FRONTEND_DIR = '/../frontend';
 
 app.get('/', (req, res) => {
-  const FILE = path.join(__dirname, `${FRONTEND}/home.html`);
+  const FILE = path.join(__dirname, `${FRONTEND_DIR}/home.html`);
+  res.sendFile(FILE);
+});
+
+app.get('/style.css', (req, res) => {
+  const FILE = path.join(__dirname, `${FRONTEND_DIR}/style.css`);
+  res.sendFile(FILE);
+});
+
+app.get('/script.js', (req, res) => {
+  const FILE = path.join(__dirname, `${FRONTEND_DIR}/script.js`);
   res.sendFile(FILE);
 });
 
