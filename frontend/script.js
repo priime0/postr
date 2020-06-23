028b8089952f61224d3037cfade6e9566b2c6adc
Priime <lucas.stamaria@gmail.com>
2020-06-23 22:38:03 +0800

    add logged-in/logged-out base, store auth in cookies, get feed to frontend

---
+++ frontend/script.js
@@ -0,0 +1,79 @@
+window.addEventListener('load', (event) => {
+  let cookies = document.cookie;
+  if (cookies === undefined || getCookie('auth') === undefined) {
+    postFeed.loggedin= false;
+  }
+  else {
+    postFeed.loggedin = true;
+    const uuid = getCookie('auth');
+    fetchFeed(uuid);
+  }
+});
+
+const postFeed = new Vue({
+  el: '#posts',
+  data: {
+    loggedin: false,
+    feed: []
+  }
+});
+
+Vue.component('post', {
+  props: ['info'],
+  template: 
+    `<div class="post">
+       <h3 class="post-title">{{ info.title }}</h3>
+       <h4 class="post-author">{{ info.author_name }} @{{ info.author_username }}</h4>
+       <h4 class="post-time">{{ info.time }}</h4>
+       <h5 class="post-tags" v-for="tag in info.tags">{{ tag }}</h5>
+       <p class="post-text">{{ info.text.substring(0, 500) }}...</p>
+       <a href="localhost:3000/post/{{ info.id }}">read more</a>
+     </div>`
+});
+
+const updateFeed = () => {
+  getCookie('auth')
+    .then(uuid => {
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+const fetchFeed = (uuid) => {
+  const URL = `http://localhost:3000/api/feed?auth=${uuid}`;
+  fetch(URL)
+    .then(data => {
+      data.json()
+        .then(posts => {
+          postFeed.feed = posts;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+const getCookie = (name) => {
+  let cookies = document.cookie.split(';');
+  let value = undefined;
+  for(let i = 0; i < cookies.length; i++) {
+    const nameVal = cookies[i].trim();
+    if (nameVal.indexOf(name) === 0) {
+      value = nameVal.substring(name.length + 1);
+    }
+  }
+  return value;
+};
+
+const setCookie = (name, value, days) => {
+  let expires = "";
+  const date = new Date();
+  date.setTime(date.getTime() + days*24*60*60*1000);
+  expires += `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}; ${expires}`;
+};
