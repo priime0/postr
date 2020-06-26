159832668586f956d7dcfa7b0937b1924f360e9c
Priime <lucas.stamaria@gmail.com>
2020-06-26 20:14:01 +0800

    add post script

---
+++ frontend/post.js
@@ -0,0 +1,140 @@
+const BASE_URL = 'http://localhost:3000';
+
+window.addEventListener('load', (event) => {
+  let cookies = document.cookie;
+  if (cookies === undefined || getCookie('auth') === undefined) {
+    postDetails.loggedin = false;
+    updatePost();
+  }
+  else {
+    postDetails.loggedin = true;
+    updatePost();
+  }
+});
+
+Vue.component('detailedpost', {
+  props: ['info'],
+  template:
+  `<h1>{{ info.title }}</h1>
+   <h5 class="author">{{ info.author_name }} @{{ info.author_username }} {{ info.time }}</h5>
+   <h5 class="tags"><span v-for"tag in info.tags">{{ tag }}</span></h5>
+   <p>{{ info.text }}</p>`
+});
+
+const postDetails = new Vue({
+  el: '#post-long',
+  data: {
+    loggedin: false,
+    info: {
+      "id": undefined,
+      "title": "",
+      "author_name": "",
+      "author_username": "",
+      "time": "",
+      "tags": [],
+      "text": ""
+    }
+  }
+});
+
+const updatePost = () => {
+  console.log('Updating post');
+  const token = getCookie('auth');
+  console.log(`Cookie: ${token}`);
+  if (token === undefined) {
+    fetchPost()
+      .then(post => {
+        console.log(`Post ${JSON.stringify(post)}`);
+        if (post.error === undefined) {
+          postDetails.info = post;
+        }
+        else {
+
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  else {
+    fetchPostAuth(token)
+      .then(post => {
+        console.log(`Post ${JSON.stringify(post)}`);
+        if (post.error === undefined) {
+          postDetails.info = post;
+        }
+        else {
+
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
+
+const fetchPostAuth = (token) => {
+  const POST_ID = getPostId();
+  const URL = `${BASE_URL}/api/post/${POST_ID}?auth=${token}`;
+  return new Promise((resolve, reject) => {
+    fetch(URL)
+      .then(data => {
+        data.json()
+          .then(post => {
+            resolve(post);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+};
+
+const fetchPost = () => {
+  const POST_ID = getPostId();
+  const URL = `${BASE_URL}/api/post/${POST_ID}`;
+  return new Promise((resolve, reject) => {
+    fetch(URL)
+      .then(data => {
+        data.json()
+          .then(post => {
+            resolve(post);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+const getPostId = () => {
+  const full = window.location.pathname;
+  const sections = full.split('/');
+  return sections[sections.length - 1];
+};
+
+const getCookie = (name) => {
+  let cookies = document.cookie.split(';');
+  let value = undefined;
+  for(let i = 0; i < cookies.length; i++) {
+    const nameVal = cookies[i].trim();
+    if (nameVal.indexOf(name) === 0) {
+      value = nameVal.substring(name.length + 1);
+    }
+  }
+  return value;
+};
+
+const setCookie = (name, value, days) => {
+  let expires = "";
+  const date = new Date();
+  date.setTime(date.getTime() + days*24*60*60*1000);
+  expires += `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value}; ${expires}`;
+};
